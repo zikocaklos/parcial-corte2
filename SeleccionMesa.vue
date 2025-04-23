6f1dca88785f1b67953a661d9599e0005d15a29f
zikocaklos <carlos321p29@gmail.com>
2025-04-22 19:06:04 -0500

    Add files via upload

---
+++ SeleccionMesa.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-4">
+    <label>Mesa disponible:</label>
+    <select :value="mesaSeleccionada" @change="e => emit('update:mesaSeleccionada', e.target.value)">
+      <option disabled value="">Selecciona una mesa</option>
+      <option v-for="mesa in mesasDisponibles" :key="mesa" :value="mesa">
+        Mesa {{ mesa }}
+      </option>
+    </select>
+
+    <p class="mt-2">Mesa seleccionada: {{ mesaSeleccionada }}</p>
+  </div>
+</template>
+
+<script setup>
+const { mesaSeleccionada } = defineProps(['mesaSeleccionada'])
+const emit = defineEmits(['update:mesaSeleccionada'])
+
+const mesasDisponibles = [1, 2, 3, 4, 5]
+</script>
